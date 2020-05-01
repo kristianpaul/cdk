fc3370f3fb8250da2614c157ed4e87bf1eaf96e8
Cristian Paul Penaranda Rojas <paul@kristianpaul.org>
2020-04-30 22:19:29 -0500

    SSM role for management

--- ec2/ec2_stack.py
+++ ec2/ec2_stack.py
@@ -21,8 +21,13 @@ class Ec2Stack(core.Stack):
             storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
             )
 
+        role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
+
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2RoleforSSM"))
+
         instance = ec2.Instance(self, "Instance",
         instance_type=ec2.InstanceType("t3.nano"),
         machine_image=amzn_linux,
-        vpc = vpc
+        vpc = vpc,
+        role = role
         )
